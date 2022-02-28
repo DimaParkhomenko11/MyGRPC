ade0b1cc720e12a2712b549f031951a2ad8d30d0
Dmitry Parkhomenko <dmitryparkhomenko11@gmail.com>
2022-02-28 20:06:53 +0200

    Added my application

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServiceTest;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+          
+            
+            /*var chanel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(chanel);
+            
+            var input = new HelloRequest{Name = "Dima"};
+            
+            var result = await client.SayHelloAsync(input);
+
+            Console.WriteLine(result.Message);*/
+
+            var chanel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Customer.CustomerClient(chanel);
+
+            Console.WriteLine("Enter user id");
+            var value = int.Parse(Console.ReadLine()!);
+            
+            var clientRequest = new CustomerLookupModel { UserId = value };
+
+            var customer = await client.GetCustomerInfoAsync(clientRequest);
+            
+            Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServiceTest/Services/CustomersService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServiceTest.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        
+        public CustomersService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            var output = new CustomerModel();
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Dima";
+                output.LastName = "Parkhomenko";
+            }
+            else if (request.UserId == 1)
+            {
+                output.FirstName = "John";
+                output.LastName = "Kekolynsky";
+            }
+            
+            return Task.FromResult(output);
+        }
+    }
+}
